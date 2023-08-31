c3359f65f8e359dde3fec79d9c616430563a5f29
Anthony Cox <anthonyjohnc@gmail.com>
2023-08-31 16:28:18 +0100

    update unit test configuration

--- tests/jest-config/unit.config.js
+++ tests/jest-config/unit.config.js
@@ -5,12 +5,12 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.js',
   ],
-  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  coverageDirectory: '<rootDir>/tests/unit/coverage/',
   coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules',
+    '<rootDir>/node_modules/',
     '<rootDir>/src/index.js',
-    '<rootDir>/tests/jest-config',
-    '<rootDir>/tests/unit/coverage',
+    '<rootDir>/tests/jest-config/',
+    '<rootDir>/tests/unit/coverage/',
   ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/jest-config/mocks/imageMock.js',
@@ -25,7 +25,7 @@ module.exports = {
     '<rootDir>/tests/unit/**/*.js'
   ],
   testPathIgnorePatterns: [
-    '<rootDir>/tests/unit/coverage',
+    '<rootDir>/tests/unit/coverage/',
   ],
   verbose: true
 };
